efffda5c5a89e61356789997645a51ed43c1f6f2
Maik Möwes <moewes@users.noreply.github.com>
2021-01-07 13:23:12 +0100

    New Ui5 Components and JavaDoc

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import "@ui5/webcomponents/dist/Avatar.js";
 import "@ui5/webcomponents/dist/Button";
 import "@ui5/webcomponents/dist/Icon";
 import "@ui5/webcomponents/dist/Label";
 import "@ui5/webcomponents/dist/Input";
 import "@ui5/webcomponents/dist/Link";
+import "@ui5/webcomponents/dist/Timeline";
+import "@ui5/webcomponents/dist/TimelineItem";
 
 import "@ui5/webcomponents-icons/dist/icons/add-document"
 import "@ui5/webcomponents-icons/dist/icons/add-equipment"
+import "@ui5/webcomponents-icons/dist/icons/employee"
+import "@ui5/webcomponents-icons/dist/icons/calendar"
