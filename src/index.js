fdb9d2181a90c720c20195219e54870fa2d24f34
Maik Möwes <moewes@users.noreply.github.com>
2021-01-08 22:17:51 +0100

    New Components and JavaDoc

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import "@ui5/webcomponents/dist/Icon";
 import "@ui5/webcomponents/dist/Label";
 import "@ui5/webcomponents/dist/Input";
 import "@ui5/webcomponents/dist/Link";
-import "@ui5/webcomponents/dist/Timeline";
-import "@ui5/webcomponents/dist/TimelineItem";
+import "@ui5/webcomponents/dist/Panel";
+import "@ui5/webcomponents-fiori/dist/Timeline";
+import "@ui5/webcomponents/dist/Title";
 
-import "@ui5/webcomponents-icons/dist/icons/add-document"
-import "@ui5/webcomponents-icons/dist/icons/add-equipment"
-import "@ui5/webcomponents-icons/dist/icons/employee"
-import "@ui5/webcomponents-icons/dist/icons/calendar"
+import "@ui5/webcomponents-icons/dist/Assets.js";
