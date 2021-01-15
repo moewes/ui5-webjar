3451e0e0faa8b7bc9b4f99c2b617585917aedc04
Maik Möwes <moewes@users.noreply.github.com>
2021-01-16 00:00:33 +0100

    new web components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import "@ui5/webcomponents/dist/Avatar.js";
 import "@ui5/webcomponents/dist/AvatarGroup.js";
+import "@ui5/webcomponents/dist/Badge";
+import "@ui5/webcomponents-fiori/dist/Bar";
 import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents/dist/Calendar";
+import "@ui5/webcomponents/dist/Card";
+import "@ui5/webcomponents/dist/Carousel";
+import "@ui5/webcomponents/dist/CheckBox";
+import "@ui5/webcomponents/dist/ComboBox";
 import "@ui5/webcomponents/dist/Icon";
 import "@ui5/webcomponents/dist/Label";
 import "@ui5/webcomponents/dist/Input";
