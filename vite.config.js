950531d11b96a398abac3bdee99f37e35980f110
Maik <moewes@users.noreply.github.com>
2023-05-27 13:22:06 +0200

    Update to 1.11.0

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: "target/classes/META-INF/resources/webjars/cloud-ui-ui5/0.4.0",
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'ui5-wc-lib',
+      // the proper extensions will be added
+      fileName: 'ui5-wc-lib',
+    },
+  },
+})
