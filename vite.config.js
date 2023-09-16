6e3eaf50ae16265e56d3772657d9fb6d0513cf17
Maik <moewes@users.noreply.github.com>
2023-09-16 11:45:23 +0200

    Update to ui5 webcomponents 1.17.0

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    outDir: "target/classes/META-INF/resources/webjars/cloud-ui-ui5/0.4.0",
+    outDir: "target/classes/META-INF/resources/webjars/cloud-ui-ui5/0.4.1",
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.js'),
